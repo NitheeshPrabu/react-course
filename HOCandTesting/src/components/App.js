da7595661a022703b4b109c12b1efdb71dbcc9c0
NitheeshPrabu <nitheeshprabu.s@gmail.com>
2019-10-05 16:12:31 +0530

    added auth HOC

--- HOCandTesting/src/components/App.js
+++ HOCandTesting/src/components/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-export default () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
-};
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>;
+    }
+    return <button onClick={() => this.props.changeAuth(true)}>Sign In</button>;
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ auth }) => ({ auth });
+
+export default connect(
+  mapStateToProps,
+  actions
+)(App);
---
+++ HOCandTesting/src/components/requireAuth.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default ChildComponent => {
+  class ComposedComponent extends Component {
+    componentDidMount() {
+      this.shouldRedirect();
+    }
+
+    componentDidUpdate() {
+      this.shouldRedirect();
+    }
+
+    shouldRedirect() {
+      if (!this.props.auth) {
+        this.props.history.push('/');
+      }
+    }
+
+    render() {
+      return <ChildComponent {...this.props} />;
+    }
+  }
+
+  const mapStateToProps = ({ auth }) => ({ auth });
+
+  return connect(mapStateToProps)(ComposedComponent);
+};
