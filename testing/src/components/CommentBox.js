72b6881b087b9752ff7d11f36dc80bf93436b3b9
NitheeshPrabu <nitheeshprabu.s@gmail.com>
2019-10-05 15:04:38 +0530

    tests for commentlist

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -13,7 +13,7 @@ class CommentBox extends Component {
 
   handleSumbit = event => {
     event.preventDefault();
-
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   };
 
