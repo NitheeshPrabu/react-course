c606e01d7e48dbbf539cdfc293ff643ea3fb17a2
NitheeshPrabu <nitheeshprabu.s@gmail.com>
2019-10-05 13:58:53 +0530

    added redux

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = {
@@ -28,4 +30,7 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(
+  null,
+  actions
+)(CommentBox);
